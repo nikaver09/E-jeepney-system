7dcd51ec366dd5ed5bd8fe69faee8a691ed0584f
nikaver09 <nicosloyd.avergonzado@hcdc.edu.ph>
2026-05-07 18:03:27 +0800

    Fix responsive mobile app and deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 })
