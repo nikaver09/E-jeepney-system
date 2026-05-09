55d56916077fbf32c0f6d29b49ece3fe9018ce67
nikaver09 <nicosloyd.avergonzado@hcdc.edu.ph>
2026-05-10 00:22:45 +0800

    Updated TrackingSection and RoutesSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './index.css'
+
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import BookingSection from './components/booking-temp'
@@ -9,28 +10,38 @@ import AboutSection from './components/about-temp'
 function App() {
   return (
     <div className="app-container">
+
+      {/* NAVBAR */}
       <Navbar />
 
+      {/* MAIN CONTENT */}
       <main className="mobile-layout">
+
+        {/* HERO */}
         <section className="section-spacing">
           <Hero />
         </section>
 
+        {/* BOOKING */}
         <section className="section-spacing">
           <BookingSection />
         </section>
 
+        {/* LIVE TRACKING */}
         <section className="section-spacing">
           <TrackingSection />
         </section>
 
+        {/* ROUTES */}
         <section className="section-spacing">
           <RoutesSection />
         </section>
 
+        {/* ABOUT */}
         <section className="section-spacing">
           <AboutSection />
         </section>
+
       </main>
     </div>
   )
